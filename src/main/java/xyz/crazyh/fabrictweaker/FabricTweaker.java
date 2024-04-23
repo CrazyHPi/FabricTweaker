92ba8cda6fb980b65a823882c0c67e3656bfcf8c
CrazyHPi <crazyhpi@outlook.com>
2024-04-23 21:30:45 +0800

    init

---
+++ src/main/java/xyz/crazyh/fabrictweaker/FabricTweaker.java
@@ -0,0 +1,10 @@
+package xyz.crazyh.fabrictweaker;
+
+import net.fabricmc.api.ModInitializer;
+
+public class FabricTweaker implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
