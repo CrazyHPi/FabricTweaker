16de8ba31c5846b2396d9524350e8e35e5f17633
CrazyHPi <crazyhpi@outlook.com>
2024-05-31 16:28:39 +0800

    fix some random stuff

--- src/main/java/xyz/crazyh/fabrictweaker/config/Callbacks.java
+++ src/main/java/xyz/crazyh/fabrictweaker/config/Callbacks.java
@@ -1,21 +1,21 @@
 package xyz.crazyh.fabrictweaker.config;
 
 import fi.dy.masa.malilib.config.options.ConfigHotkey;
-import fi.dy.masa.malilib.hotkeys.IHotkeyCallback;
 import net.minecraft.client.MinecraftClient;
 import xyz.crazyh.fabrictweaker.gui.GuiConfigs;
 import xyz.crazyh.fabrictweaker.utils.InventoryUtils;
+import xyz.crazyh.fabrictweaker.utils.RandomUtils;
 
 public class Callbacks {
     public static void init(MinecraftClient mc) {
-        setHotkeyCallback(Hotkeys.OPEN_GUI, GuiConfigs::openGui, true);
-        setHotkeyCallback(Hotkeys.DROP_INVENTORY, InventoryUtils::dropInv, true);
+        setHotkeyCallback(Hotkeys.OPEN_GUI, GuiConfigs::openGui);
+        setHotkeyCallback(Hotkeys.DROP_INVENTORY, InventoryUtils::dropInv);
     }
 
-    public static void setHotkeyCallback(ConfigHotkey hotkey, Runnable run, boolean cancel) {
+    public static void setHotkeyCallback(ConfigHotkey hotkey, Runnable run) {
         hotkey.getKeybind().setCallback((action, key) -> {
             run.run();
-            return cancel;
+            return true;
         });
     }
 }
---
+++ src/main/java/xyz/crazyh/fabrictweaker/utils/RandomUtils.java
@@ -0,0 +1,8 @@
+package xyz.crazyh.fabrictweaker.utils;
+
+import fi.dy.masa.malilib.config.options.ConfigString;
+import net.minecraft.client.MinecraftClient;
+
+public class RandomUtils {
+
+}
