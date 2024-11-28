71ce6c1d10ee4c21a0a7677930555a0871935e14
CrazyHPi <crazyhpi@outlook.com>
2024-11-28 15:39:28 +0800

    check Z axis instead of Y

--- src/main/java/xyz/crazyh/fabrictweaker/utils/BlockUtils.java
+++ src/main/java/xyz/crazyh/fabrictweaker/utils/BlockUtils.java
@@ -1,7 +1,6 @@
 package xyz.crazyh.fabrictweaker.utils;
 
 import net.minecraft.block.*;
-import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
@@ -12,7 +11,8 @@ public class BlockUtils {
 
         for (int i = -1; i < 2; i++) {
             for (int j = -1; j < 2; j++) {
-                Block block = player.getWorld().getBlockState(pos.offset(Direction.Axis.X, i).offset(Direction.Axis.Y, j)).getBlock();
+//                BlockPos offset = pos.offset(Direction.Axis.X, i).offset(Direction.Axis.Z, j);
+                Block block = player.getWorld().getBlockState(pos.offset(Direction.Axis.X, i).offset(Direction.Axis.Z, j)).getBlock();
                 if (block instanceof FenceBlock || block instanceof WallBlock || block instanceof FenceGateBlock) {
                     return true;
                 }
