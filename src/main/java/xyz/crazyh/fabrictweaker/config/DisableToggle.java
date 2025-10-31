46f88debd460ce5e515296c8ef2486d0b77f0e8e
CrazyHPi <crazyhpi@outlook.com>
2025-10-31 22:53:59 +0800

    add disable locator bar option

--- src/main/java/xyz/crazyh/fabrictweaker/config/DisableToggle.java
+++ src/main/java/xyz/crazyh/fabrictweaker/config/DisableToggle.java
@@ -25,6 +25,7 @@ public enum DisableToggle implements IHotkeyTogglable, IConfigNotifiable<IConfig
     DISABLE_BOSS_DARKEN_SKY("Disable Boss Darken Sky", false, "Disable darken sky when boss(wither) near by"),
     DISABLE_EXPLOSION_SOUND("Disable Explosion Sound", false, "Disable explosion sounds caused by tnt, creeper, wither skull"),
     DISABLE_LEVITATION("Disable Levitation", false, "Disable LEVITATION effect, basically cheating"),
+    DISABLE_LOCATOR_BAR("Disable Locator Bar", false, "Always display experience bar"),
     DISABLE_PLAYER_SLOWDOWN("Disable Player Slowdown", false, "Disable player related slowdown"),
     DISABLE_PARTIAL_COMMAND_FEEDBACK("Disable Partial Command Feedback", false, "Disable SOME command feedback, only works in single player"),
     DISABLE_SNEAK_SLOWDOWN("Disable Sneak Slowdown", false, "Disable slowdown effect when sneaking."),
---
+++ src/main/java/xyz/crazyh/fabrictweaker/mixin/Disables/disableLocatorBar/InGameHudMixin.java
@@ -0,0 +1,21 @@
+package xyz.crazyh.fabrictweaker.mixin.Disables.disableLocatorBar;
+
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import net.minecraft.client.gui.hud.InGameHud;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import xyz.crazyh.fabrictweaker.config.DisableToggle;
+
+@Mixin(InGameHud.class)
+public abstract class InGameHudMixin {
+    @ModifyExpressionValue(
+            method = "getCurrentBarType",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/gui/hud/InGameHud;shouldShowExperienceBar()Z"
+            )
+    )
+    private boolean shouldShowExperienceBar(boolean original) {
+        return DisableToggle.DISABLE_LOCATOR_BAR.getBooleanValue() || original;
+    }
+}
