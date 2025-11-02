21a30e2e57b44fc624823f4da46e5fb9ee6984ac
CrazyHPi <crazyhpi@outlook.com>
2025-11-02 23:29:15 +0800

    fix disableSneakSlowdown

--- src/main/java/xyz/crazyh/fabrictweaker/mixin/Disables/disableSneakSlowdown/ClientPlayerEntityMixin.java
+++ src/main/java/xyz/crazyh/fabrictweaker/mixin/Disables/disableSneakSlowdown/ClientPlayerEntityMixin.java
@@ -1,5 +1,6 @@
 package xyz.crazyh.fabrictweaker.mixin.Disables.disableSneakSlowdown;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import net.minecraft.client.network.ClientPlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -8,17 +9,17 @@ import xyz.crazyh.fabrictweaker.config.DisableToggle;
 
 @Mixin(ClientPlayerEntity.class)
 public abstract class ClientPlayerEntityMixin {
-    @Redirect(
-            method = "tickMovement",
+    @ModifyExpressionValue(
+            method = "applyMovementSpeedFactors",
             at = @At(
                     value = "INVOKE",
                     target = "Lnet/minecraft/client/network/ClientPlayerEntity;shouldSlowDown()Z"
             )
     )
-    private boolean cancelSlowdown(ClientPlayerEntity instance) {
+    private boolean cancelSlowdown(boolean original) {
         if (DisableToggle.DISABLE_SNEAK_SLOWDOWN.getBooleanValue()) {
             return false;
         }
-        return instance.shouldSlowDown();
+        return original;
     }
 }
