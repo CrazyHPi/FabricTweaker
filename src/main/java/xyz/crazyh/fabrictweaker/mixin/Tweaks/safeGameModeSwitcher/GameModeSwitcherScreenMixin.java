e4b6c0f9ae9491b79366df70e0ea3cc3085fee71
CrazyHPi <crazyhpi@outlook.com>
2026-01-18 14:53:05 +0800

    safe game mode switcher

---
+++ src/main/java/xyz/crazyh/fabrictweaker/mixin/Tweaks/safeGameModeSwitcher/GameModeSwitcherScreenMixin.java
@@ -0,0 +1,39 @@
+package xyz.crazyh.fabrictweaker.mixin.Tweaks.safeGameModeSwitcher;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.GameModeSwitcherScreen;
+import net.minecraft.client.gui.screen.GameModeSwitcherScreen.GameModeSelection;
+import net.minecraft.network.packet.c2s.play.ChangeGameModeC2SPacket;
+import net.minecraft.world.GameMode;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import xyz.crazyh.fabrictweaker.config.FeatureToggle;
+
+@Mixin(GameModeSwitcherScreen.class)
+public abstract class GameModeSwitcherScreenMixin {
+
+    @Inject(
+            method = "apply(Lnet/minecraft/client/MinecraftClient;Lnet/minecraft/client/gui/screen/GameModeSwitcherScreen$GameModeSelection;)V",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/client/network/ClientPlayNetworkHandler;sendPacket(Lnet/minecraft/network/packet/Packet;)V"
+            ),
+            cancellable = true
+    )
+    private static void onlySwitchSurvivalAndSpector(MinecraftClient client, GameModeSelection newGameMode, CallbackInfo ci) {
+        if (FeatureToggle.SAFE_GAME_MODE_SWITCHER.getBooleanValue()) {
+            GameMode currGameMode = client.interactionManager.getCurrentGameMode();
+            client.player.networkHandler.sendPacket(new ChangeGameModeC2SPacket(
+                    switch (currGameMode) {
+                        case SURVIVAL -> GameMode.SPECTATOR;
+                        case SPECTATOR -> GameMode.SURVIVAL;
+                        case CREATIVE, ADVENTURE -> newGameMode.gameMode;
+                    }
+            ));
+
+            ci.cancel();
+        }
+    }
+}
--- src/main/resources/fabrictweaker.aw
+++ src/main/resources/fabrictweaker.aw
@@ -1,1 +1,3 @@
 classTweaker v1 named
+accessible class net/minecraft/client/gui/screen/GameModeSwitcherScreen$GameModeSelection
+accessible field net/minecraft/client/gui/screen/GameModeSwitcherScreen$GameModeSelection gameMode Lnet/minecraft/world/GameMode;
