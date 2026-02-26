f28764365e43ec647448ee08d94a7113b797ff5e
CrazyHPi <crazyhpi@outlook.com>
2026-02-26 16:32:53 +0800

    nuke @Redirect

--- src/main/java/xyz/crazyh/fabrictweaker/mixin/Disables/disableBossDarkenSky/GameRenderMixin.java
+++ src/main/java/xyz/crazyh/fabrictweaker/mixin/Disables/disableBossDarkenSky/GameRenderMixin.java
@@ -1,26 +1,25 @@
 package xyz.crazyh.fabrictweaker.mixin.Disables.disableBossDarkenSky;
 
-import net.minecraft.client.gui.hud.BossBarHud;
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import net.minecraft.client.render.GameRenderer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import xyz.crazyh.fabrictweaker.config.DisableToggle;
 
 @Mixin(GameRenderer.class)
 public abstract class GameRenderMixin {
 
-    @Redirect(
+    @ModifyExpressionValue(
             method = "tick",
             at = @At(
                     value = "INVOKE",
                     target = "Lnet/minecraft/client/gui/hud/BossBarHud;shouldDarkenSky()Z"
             )
     )
-    private boolean noDarkenSky(BossBarHud instance) {
+    private boolean noDarkenSky(boolean original) {
         if (DisableToggle.DISABLE_BOSS_DARKEN_SKY.getBooleanValue()){
             return false;
         }
-        return instance.shouldDarkenSky();
+        return original;
     }
 }
--- src/main/java/xyz/crazyh/fabrictweaker/mixin/Disables/disableMinihudPacketWarning/ServuxStructuresPacketMixin.java
+++ src/main/java/xyz/crazyh/fabrictweaker/mixin/Disables/disableMinihudPacketWarning/ServuxStructuresPacketMixin.java
@@ -1,15 +1,15 @@
 package xyz.crazyh.fabrictweaker.mixin.Disables.disableMinihudPacketWarning;
 
+import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
 import fi.dy.masa.minihud.network.ServuxStructuresPacket;
 import org.apache.logging.log4j.Logger;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import xyz.crazyh.fabrictweaker.config.DisableToggle;
 
 @Mixin(ServuxStructuresPacket.class)
 public abstract class ServuxStructuresPacketMixin {
-    @Redirect(
+    @WrapWithCondition(
             method = "fromPacket",
             at = @At(
                     value = "INVOKE",
@@ -17,14 +17,34 @@ public abstract class ServuxStructuresPacketMixin {
                     remap = false
             )
     )
-    private static void noInvalidWarn(Logger instance, String s) {
-        if (DisableToggle.DISABLE_MINIHUD_PACKET_WARNING.getBooleanValue()) {
-            return;
-        }
-        instance.warn(s);
+    private static boolean noInvalidWarn(Logger instance, String s) {
+        return !DisableToggle.DISABLE_MINIHUD_PACKET_WARNING.getBooleanValue();
     }
 
-    @Redirect(
+    @WrapWithCondition(
+            method = "fromPacket",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lorg/apache/logging/log4j/Logger;error(Ljava/lang/String;Ljava/lang/Object;)V",
+                    remap = false
+            )
+    )
+    private static boolean noError(Logger instance, String s, Object o) {
+        return !DisableToggle.DISABLE_MINIHUD_PACKET_WARNING.getBooleanValue();
+    }
+
+    @WrapWithCondition(
+            method = "fromPacket",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lorg/apache/logging/log4j/Logger;error(Ljava/lang/String;Ljava/lang/Throwable;)V"
+            )
+    )
+    private static boolean noCatchError(Logger instance, String s, Throwable throwable) {
+        return !DisableToggle.DISABLE_MINIHUD_PACKET_WARNING.getBooleanValue();
+    }
+
+    @WrapWithCondition(
             method = "fromPacket",
             at = @At(
                     value = "INVOKE",
@@ -32,10 +52,19 @@ public abstract class ServuxStructuresPacketMixin {
                     remap = false
             )
     )
-    private static void noFinalError(Logger instance, String s) {
-        if (DisableToggle.DISABLE_MINIHUD_PACKET_WARNING.getBooleanValue()) {
-            return;
-        }
-        instance.warn(s);
+    private static boolean noFinalError(Logger instance, String s) {
+        return !DisableToggle.DISABLE_MINIHUD_PACKET_WARNING.getBooleanValue();
+    }
+
+    @WrapWithCondition(
+            method = "toPacket",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lorg/apache/logging/log4j/Logger;error(Ljava/lang/String;Ljava/lang/Object;)V",
+                    remap = false
+            )
+    )
+    private static boolean noToPacketError(Logger instance, String s, Object o) {
+        return !DisableToggle.DISABLE_MINIHUD_PACKET_WARNING.getBooleanValue();
     }
 }
