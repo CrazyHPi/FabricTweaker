7944743f92b4ce7c161a99afdc27efaef678bde5
CrazyHPi <crazyhpi@outlook.com>
2026-02-26 14:35:42 +0800

    make DISABLE_DAMAGE_FLINCH actually works

--- src/main/java/xyz/crazyh/fabrictweaker/mixin/Disables/disableDamageFlinch/GameRendererMixin.java
+++ src/main/java/xyz/crazyh/fabrictweaker/mixin/Disables/disableDamageFlinch/GameRendererMixin.java
@@ -1,17 +1,17 @@
 package xyz.crazyh.fabrictweaker.mixin.Disables.disableDamageFlinch;
 
-import net.minecraft.client.network.ClientPlayNetworkHandler;
-import net.minecraft.network.packet.s2c.play.DamageTiltS2CPacket;
+import net.minecraft.client.render.GameRenderer;
+import net.minecraft.client.util.math.MatrixStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import xyz.crazyh.fabrictweaker.config.DisableToggle;
 
-@Mixin(ClientPlayNetworkHandler.class)
-public abstract class ClientPlayNetworkHandlerMixin {
-    @Inject(method = "onDamageTilt", at = @At("HEAD"), cancellable = true)
-    private void cancelFlinch(DamageTiltS2CPacket packet, CallbackInfo ci) {
+@Mixin(GameRenderer.class)
+public abstract class GameRendererMixin {
+    @Inject(method = "tiltViewWhenHurt", at = @At("HEAD"), cancellable = true)
+    private void cancelTile(MatrixStack matrices, float tickProgress, CallbackInfo ci) {
         if (DisableToggle.DISABLE_DAMAGE_FLINCH.getBooleanValue()) {
             ci.cancel();
         }
